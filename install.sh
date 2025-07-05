982ef14d17b0d93215f9ea66737cb65a6b0728dd
Łukasz Szmelc <lszmelc@MacBook-Pro-ukasz.local>
2025-07-05 13:24:01 +0200

    Updates

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@
 # brew tap caskroom/cask
 # brew tap caskroom/versions
 
-# brew install node
-# brew install git
+brew install node
+brew install git
+brew install --cask visual-studio-code
+brew install --cask postman
+brew install redis
+brew install --cask spotify
+brew install --cask vlc
+brew install --cask docker
 
 # # Install zsh and make it default shell
 # brew install zsh
 # sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # brew install --cask google-chrome
-# brew install --cask spotify
-# brew install --cask vlc
 
 ln -sv “~/.dotfiles/runcom/.bash_profile” ~
 ln -sv “~/.dotfiles/runcom/.inputrc” ~
